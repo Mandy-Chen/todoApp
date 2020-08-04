08cf17d97b30db92a026fb541f99d425dbbd8009
Mandy <chenjiafeng16@126.com>
2020-08-04 21:02:20 +0800

    [todoApp][mandy] feature :Add delete Icon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './todo.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 class Todo extends React.Component {
     constructor(props) {
         super(props)
     }
     render() {
         return (
-        <div className="todo">{this.props.text}</div>
+            <div className="todo">{this.props.text}
+                <div>
+                    <i className="fa fa-trash" aria-hidden="true"></i>
+                </div>
+            </div>
         )
     }
 }
