b76c8e372adc0c18c60a747fb3632a371ac8b2ab
Mandy <chenjiafeng16@126.com>
2020-08-05 00:22:28 +0800

    [todoApp][mandy] reafctor :implement mark text

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,10 +4,22 @@ import '../../node_modules/font-awesome/css/font-awesome.min.css'
 class Todo extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            mark: false,
+          }
     }
+    doMark(index) {
+        //拿到某个数据 this.props.todos[index]
+    //    console.log(this.props.todos[index])
+       console.log("this"+index)
+        this.setState({
+          mark: !this.state.mark
+      })
+      }
     render() {
         return (
-            <div className="todo">{this.props.text}
+            <div className={this.state.mark ? "todoMark" : "todoUnmark"} 
+            onClick={this.doMark.bind(this)}>{this.props.text}
             </div>
         )
     }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,26 +4,19 @@ import './todo.css';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      index: []
-    }
+    
   }
   deleteText(index) {
-    let todoList = this.props.todos;
-    // todoList.splice(index, 1);
     this.props.deleteTodo(index);
-    let list=
-    this.setState={
-      index:this.state.index.push(index)
-    }
   }
 
   render() {
     return (
       <div>
+      
         {this.props.todos.map((todo, index) => (
           <div className="todoList" key={index} >
-            <Todo text={todo}></Todo>
+              <Todo text={todo} ></Todo>
             <button onClick={this.deleteText.bind(this, index)} >
               <i className="fa fa-trash" aria-hidden="true"></i>
             </button>
